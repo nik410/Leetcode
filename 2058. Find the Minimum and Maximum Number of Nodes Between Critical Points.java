47953fe2482f97451f2b9f4fc1d7d104f0c7170a
Niket Pandey <107863847+nik410@users.noreply.github.com>
2024-07-05 20:27:09 +0530

    Create 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.java

---
+++ 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        
+        ArrayList<Integer> al = new ArrayList();
+
+        ListNode prev = head;
+        ListNode l1 = head.next;
+        int index = 2;
+
+        while(l1.next != null){
+            if(( prev.val > l1.val && l1.next.val > l1.val ) || ( prev.val < l1.val && l1.next.val < l1.val )){
+                al.add(index);
+            }
+            prev = l1;
+            l1 = l1.next;
+            index++;
+        }
+
+
+        System.out.println(al);
+
+        if(al.size() < 2)
+            return new int[]{-1, -1};
+        else{
+            Collections.sort(al);
+
+            int ans[] = new int[2];
+            ans[1] = al.get(al.size() - 1) - al.get(0);
+
+            int min = Integer.MAX_VALUE;
+            for(int i = 0; i < al.size() - 1; i ++){
+                min = Math.min(al.get(i + 1) - al.get(i), min);
+            }
+
+            ans[0] = min;
+
+            return ans;
+        }
+    }
+}
